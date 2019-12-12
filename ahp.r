a5b4f62615090b1d50fbeafdea83d4517f725bae
falahudinhs <falahudin6@gmail.com>
2019-12-12 14:32:45 +0700

    ahp

--- ahp.r
+++ ahp.r
@@ -5,24 +5,43 @@
 # sumberDaya        1/4         1           1/3            4
 # stakeholder        1          3            1             5
 # dukunganPublik    1/6        1/4          1/5            1
-pairwiseKriteria = c(1,1/4,4,1/6,4,1,4,1/4,1/4,1/4,1,1/5,6,4,5,1)
-jumlahEigen = sqrt(length(pairwiseKriteria))
-matKriteria = matrix(data = pairwiseKriteria, ncol = jumlahEigen, byrow = T)
-eigSebelum = numeric(jumlahEigen)
-selisihEig = rep(1, jumlahEigen)
-matOlah = matKriteria
-while (all(selisihEig > 0.0001)) {
-  eigSekarang = c()
-  matOlah = matOlah %*% matOlah
-  for (j in 1:dim(matOlah)[1]) {
-    eigSekarang[j] = sum(matOlah[j,])/sum(matOlah)
+
+pairwiseKriteria = rep(1, 16)
+pairwiseSubApproval = rep(1, 9)
+pairwiseSubSD = rep(1, 9)
+pairwiseAltAdministrasi = rep(1, 9)
+pairwiseAltHukum = rep(1, 9)
+pairwiseAltPolitik = rep(1, 9)
+pairwiseAltFinansial = rep(1, 9)
+pairwiseAltAhli = rep(1, 9)
+pairwiseAltTeknologi = rep(1, 9)
+pairwiseAltStakeholder = rep(1, 9)
+pairwiseAltDukungan = rep(1, 9)
+
+pairwise = c(1,1/4,4,1/6,4,1,4,1/4,1/4,1/4,1,1/5,6,4,5,1)
+
+nilaiEigen = function(pairwiseKriteria){
+  jumlahEigen = sqrt(length(pairwiseKriteria))
+  matKriteria = matrix(data = pairwiseKriteria, ncol = jumlahEigen, byrow = T)
+  eigSebelum = numeric(jumlahEigen)
+  selisihEig = rep(1, jumlahEigen)
+  matOlah = matKriteria
+  while (all(selisihEig > 0.0001)) {
+    eigSekarang = c()
+    matOlah = matOlah %*% matOlah
+    for (j in 1:dim(matOlah)[1]) {
+      eigSekarang[j] = sum(matOlah[j,])/sum(matOlah)
+    }
+    selisihEig = abs(eigSekarang - eigSebelum)
+    eigSebelum = eigSekarang
+    # print(eigSekarang)
   }
-  selisihEig = abs(eigSekarang - eigSebelum)
-  eigSebelum = eigSekarang
-  print(selisihEig)
+  return(eigSekarang)
 }
 
-konsistensi = function(matriksPairwise, eigen){
+konsistensi = function(pairwiseVector, eigen){
+  jumlahEigen = sqrt(length(pairwiseVector))
+  matriksPairwise = matrix(data = pairwiseVector, ncol = jumlahEigen, byrow = T)
   weightedSumVector = matriksPairwise %*% eigen
   consistencyVector = weightedSumVector/eigen
   rataan = mean(consistencyVector)
